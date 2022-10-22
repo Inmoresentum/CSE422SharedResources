d536fd38932f8a58985c89002971ca3aa852e135
inmoresentum <rafee69029@gmail.com>
2022-10-23 00:13:11 +0600

    Added None to make function and method completely type inferred, Fixed type hinting bug, reformatted the code, renamed taking_input_from_txt_file() to take_input_from_txt_file() and some minor changes.

--- src/Submission_In_Python/Lab1/Lab1.py
+++ src/Submission_In_Python/Lab1/Lab1.py
@@ -7,21 +7,23 @@ from queue import PriorityQueue
 
 
 class Node:
-    def __init__(self):
+    __slots__ = ("heuristic_value", "adj_nodes")
+
+    def __init__(self) -> None:
         self.heuristic_value: int = int(1e9)
-        self.adj_nodes: [tuple[str, int]] = []
+        self.adj_nodes: list[tuple[str, int]] = []
 
     def __str__(self) -> str:
-        return "heuristic_value " + str(self.heuristic_value) + " adj_nodes " + str(self.adj_nodes)
+        return f"""heuristic_value = {self.heuristic_value} adj_nodes = " {self.adj_nodes}"""
 
 
-graph: dict[str: Node] = {}
-distance: dict[str: int] = {}
-parent: dict[str: str] = {}
-input_lines: [str] = []
+graph: dict[str, Node] = {}
+distance: dict[str, int] = {}
+parent: dict[str, str] = {}
+input_lines: list[str] = []
 
 
-def taking_input_from_txt_file():
+def take_input_from_txt_file() -> None:
     with open("input.txt", "r") as file:
         for readline in file:
             input_lines.append(readline.strip())
@@ -33,11 +35,11 @@ def taking_input_from_txt_file():
         file.close()  # Closing the input stream
 
 
-taking_input_from_txt_file()
+take_input_from_txt_file()
 
 
 # Time to build the graph
-def build_graph():
+def build_graph() -> None:
     for _i in input_lines:
         cur_line = _i.split()
         city_name = cur_line[0]
@@ -51,11 +53,11 @@ build_graph()
 
 
 # Time to do a star search
-def a_start_search(source: str, destination: str):
+def a_start_search(source: str, destination: str) -> None:
     distance[source] = 0
     final_cost = graph.get(source).heuristic_value
     pq: PriorityQueue[tuple[int, str, int]] = PriorityQueue()
-    # putting in tuple in this format final_cost, city_name, distance/cost
+    # putting in tuple in this format -> final_cost, city_name, distance/cost
     pq.put((final_cost, source, 0))
     while not pq.empty():
         cur_final_cost, cur_visiting_node, cur_distance = pq.get()
@@ -71,14 +73,16 @@ def a_start_search(source: str, destination: str):
                 pq.put((update_final_cost, k[0], distance.get(k[0])))
 
 
-starting_node = input("Start node: ")
-ending_node = input("Destination: ")
+starting_node: str = input("Start node: ")
+ending_node: str = input("Destination: ")
 a_start_search(starting_node, ending_node)
+
+# If no path found then simply print "NO PATH FOUND" and then exit
 if distance.get(ending_node) == int(1e9):
     print("NO PATH FOUND")
     sys.exit(0)
 print(distance.get(ending_node))
-path: [str] = []
+path: list[str] = []
 cur_node = ending_node
 while cur_node != starting_node:
     path.append(cur_node)
