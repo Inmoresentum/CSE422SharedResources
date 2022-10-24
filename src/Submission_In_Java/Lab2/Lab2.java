4c1c011ec1cd136efb0de013f7a4044b5089bb92
inmoresentum <rafee69029@gmail.com>
2022-10-24 23:16:12 +0600

    Added Lab2.java file which I will complete later on.

---
+++ src/Submission_In_Java/Lab2/Lab2.java
@@ -0,0 +1,16 @@
+package Submission_In_Java.Lab2;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class Lab2 {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final PrintWriter pw = new PrintWriter(new BufferedOutputStream(System.out));
+
+
+    public static void main(String[] args) {
+
+    }
+}
